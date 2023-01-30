4c3467bebee07977d0d4b438b822ce9439cad955
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-01-31 01:48:30 +0530

    Add template to the list (header and footer)

--- app.js
+++ app.js
@@ -42,10 +42,8 @@ app.get("/work",function(req,res){
     res.render("list",{listTitle: "Work List",newitems: workItems});
 });
 
-app.post("/work",function(req,res){
-    let item = req.body.newItem;
-    workItems.push(item);
-    res.redirect("/work");
+app.get("/about",function(req,res){
+    res.render("about");
 })
 
 
