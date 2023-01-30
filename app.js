fd26923550f2d0aed8b2c33af4f3d9d59a201c4d
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-01-31 01:31:45 +0530

    Complete todo list and work list

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//variables to store items and work items
 let items = ["Buy Food","Cook Food","Eat Food"];
+let workItems = [];
 
+//root route get and post
 app.get("/",function(req,res){
     
     let today = new Date();
@@ -15,18 +18,38 @@ app.get("/",function(req,res){
     let day = "";
     let options = { weekday: 'long', month: 'long', day: 'numeric' };
     day = today.toLocaleDateString("en-US", options);
-    res.render("list",{kindOfDay: day,newitems: items});
+    res.render("list",{listTitle: day,newitems: items});
         
 });
 
+
 app.post("/",function(req,res){
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
-    //console.log(item);
+    console.log(req.body);
+    if(req.body.item === 'Work List'){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+
 });
 
 
+//work route get and post
+app.get("/work",function(req,res){
+    res.render("list",{listTitle: "Work List",newitems: workItems});
+});
+
+app.post("/work",function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+
+//starting of the page
 app.listen(3000,function(){
     console.log("Server started at port 3000");
 });
