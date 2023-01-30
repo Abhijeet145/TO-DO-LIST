8dbcf1dda9e350d9299eb99095bff283de2c5db7
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-01-31 02:35:52 +0530

    Refactoring with making custom modules

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date");
 
 const app = express();
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 //variables to store items and work items
-let items = ["Buy Food","Cook Food","Eat Food"];
-let workItems = [];
+const items = ["Buy Food","Cook Food","Eat Food"];
+const workItems = [];
 
 //root route get and post
 app.get("/",function(req,res){
     
-    let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
-    let options = { weekday: 'long', month: 'long', day: 'numeric' };
-    day = today.toLocaleDateString("en-US", options);
+    const day = date.getDate();
     res.render("list",{listTitle: day,newitems: items});
         
 });
 
 
 app.post("/",function(req,res){
-    let item = req.body.newItem;
-    console.log(req.body);
+    const item = req.body.newItem;
+
     if(req.body.item === 'Work List'){
         workItems.push(item);
         res.redirect("/work");
@@ -33,15 +31,15 @@ app.post("/",function(req,res){
         items.push(item);
         res.redirect("/");
     }
-
 });
 
 
-//work route get and post
+//work route get  
 app.get("/work",function(req,res){
     res.render("list",{listTitle: "Work List",newitems: workItems});
 });
 
+//about route get
 app.get("/about",function(req,res){
     res.render("about");
 })
